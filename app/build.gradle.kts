2339a55e4749d3ba91e7e6a13afc00b3fa0bd5b9
Denis Nek <dnek@lyft.com>
2021-04-02 22:27:06 -0700

    Add camera control

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,9 +56,10 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("com.google.android.material:material:1.2.1")
+    implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
+    implementation("androidx.compose.material:material-icons-extended:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.0-alpha06")
     implementation("androidx.activity:activity-compose:1.3.0-alpha02")
